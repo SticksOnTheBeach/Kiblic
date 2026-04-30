33a29ea8e12d4f2c23cc8d7d5df1ea69f64432a1
Mael <mael@macbook-pro-de-mael.home>
2026-04-30 21:56:48 +0200

    refactor(drive): extract OAuth2 logic into DriveAuthenticator to respect Single Responsibility Principle

--- src/com/sticksonthebeach/syncapp/main/App.java
+++ src/com/sticksonthebeach/syncapp/main/App.java
@@ -4,7 +4,10 @@ package com.sticksonthebeach.syncapp.main;
 
 import java.io.File;
 
+import com.google.api.services.drive.Drive;
 import com.sticksonthebeach.syncapp.controller.GitController;
+import com.sticksonthebeach.syncapp.model.DriveAuthenticator;
+import com.sticksonthebeach.syncapp.model.DriveManager;
 import com.sticksonthebeach.syncapp.model.GitManager;
 import com.sticksonthebeach.syncapp.util.Constants;
 import com.sticksonthebeach.syncapp.view.GitPanelView;
@@ -23,10 +26,19 @@ public class App extends Application {
 
         // View Initilisation
         GitPanelView gitView = new GitPanelView();
+        
+        // 1. On engage la sécurité pour obtenir l'accès
+        Drive googleService = DriveAuthenticator.getDriveService();
+
+        // 2. On embauche le manager en lui donnant l'accès
+        DriveManager driveManager = new DriveManager(googleService);
+
+        // Et voilà, driveManager est prêt à travailler !
 
         // 3. Initialisation des Contrôleurs (On relie Modèle et Vue)
         new GitController(gitManager, gitView);
-
+        
+        
         // 4. Configuration de la fenêtre principale
         Scene scene = new Scene(gitView, Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
         primaryStage.setTitle(Constants.APP_TITLE);
--- src/com/sticksonthebeach/syncapp/model/DriveManager.java
+++ src/com/sticksonthebeach/syncapp/model/DriveManager.java
@@ -4,26 +4,17 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.security.GeneralSecurityException;
-import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
 
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.FileContent;
-import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
 
 /**
@@ -31,41 +22,19 @@ import com.google.api.services.drive.model.File;
  */
 public class DriveManager {
 
-    // --- LES CONSTANTES DE SÉCURITÉ ---
-    private static final String APPLICATION_NAME = "Kiblic Sync App";
-    private static final JsonFactory JSON_FACTORY = GsonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";     
-    // On demande un accès total au Drive (pour créer, modifier, supprimer)
-    private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
-    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
-    private final Drive driveService;
-
+	
+	private final Drive driveService;
+	
     /**
      * Constructor : Initialize the connection and the creation of the Manager
      */
-    public DriveManager() throws IOException, GeneralSecurityException {
-        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-        Credential credential = getCredentials(HTTP_TRANSPORT);
-        this.driveService = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
-                .setApplicationName(APPLICATION_NAME)
-                .build();
-    }
-
-
-    private Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
-        InputStream is = DriveManager.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
-        if (is == null) {
-            throw new FileNotFoundException("Fichier introuvable, à l'adresse : " + CREDENTIALS_FILE_PATH);
+	public DriveManager(Drive driveService) {
+        if (driveService == null) {
+            throw new IllegalArgumentException("Drive service cannot be null. Authentication failed.");
         }
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(is));
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
+        this.driveService = driveService;
     }
+
 
     /**
      * Create a new folder to the Google Drive root.
